c78eff3eaa82305a8c4ac1c1332f33af7a7a7c1a
yhupaul <yhupaul@hotmail.com>
2021-07-29 19:48:56 -0700

    fetchIssFlyOverTime from indext.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { fetchBreedDescription } = require('../json_the_cat/breedFetcher');
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 
 fetchMyIP((error, ip) => {
@@ -20,4 +20,16 @@ fetchCoordsByIP('173.180.85.211', (error, coords) => {
   console.log('It worked! Returned coords:' , coords);
 
 
- });
+});
+const coords = { latitude: 49.2663, longitude: -122.8739 };
+fetchISSFlyOverTimes(coords, (error, passTime) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned ISS flyove time:' , passTime);
+
+
+});
+
--- iss.js
+++ iss.js
@@ -8,7 +8,7 @@
  */
 const request = require('request');
 
-const fetchMyIP = function (callback) {
+const fetchMyIP = function(callback) {
   const url = "https://api.ipify.org/?format=json";
 
   // use request to fetch IP address from JSON API
@@ -29,7 +29,7 @@ const fetchMyIP = function (callback) {
   });
 };
 
-const fetchCoordsByIP = function (ip, callback) {
+const fetchCoordsByIP = function(ip, callback) {
   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -50,4 +50,25 @@ const fetchCoordsByIP = function (ip, callback) {
 };
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP, };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching  ISS flyover time. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const passTime = JSON.parse(body).response;
+
+    callback(null, passTime);
+  });
+
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
